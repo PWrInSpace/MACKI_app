eb67ca601f6a92130fa77e70682e028a0c1beb4a
Kuba <jakkub68@gmail.com>
2024-09-11 00:24:02 +0200

    init serial

---
+++ src/com/com_proto_basic.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+class ComProtoBasic(ABC):
+    @abstractmethod
+    def connect(self):
+        """Connect to the device"""
+        pass
+
+    @abstractmethod
+    def disconnect(self):
+        """Disconnect from the device"""
+        pass
+
+    @abstractmethod
+    def write(self, data: str):
+        """Write data to the device
+
+        Args:
+            data (str): data to write
+        """
+        pass
+
+    @abstractmethod
+    def read(self):
+        """Read data from the device"""
+        pass
+
+    @abstractmethod
+    def is_connected(self):
+        """Check if the device is connected"""
+        pass
---
+++ src/com/macus_serial.py
@@ -0,0 +1,143 @@
+from typing import override
+import serial
+from serial.serialutil import PortNotOpenError, SerialException
+
+from src.com.com_proto_basic import ComProtoBasic
+
+
+class MacusSerial(ComProtoBasic):
+    EOF = "\r\n"
+    BAUDRATE = 115200
+    READ_TIMEOUT_S = 0.1
+    WRITE_TIMEOUT_S = 0.1
+
+    ACK = "ACK"
+    NACK = "NACK"
+
+    def __init__(self, com_port: str = None) -> None:
+        """ This method initializes the MacusSerial class
+
+        Args:
+            com_port (str, optional): com port to connect to. Defaults to None.
+        """
+        super().__init__()
+        self._serial = serial.Serial()
+        self._serial.port = com_port
+        self._serial.baudrate = self.BAUDRATE
+        self._serial.timeout = self.READ_TIMEOUT_S
+        self._serial.write_timeout = self.WRITE_TIMEOUT_S
+
+    @override
+    def connect(self, com_port: str = None) -> None:
+        """ This method connects to the serial port
+
+        Args:
+            com_port (str, optional): com port to connect to. Defaults to None.
+
+        Raises:
+            SerialException: Serial port is already open
+            ValueError: COM port not provided
+        """
+        if self._serial.is_open:
+            raise SerialException("Serial port is already open")
+
+        self._serial.port = com_port or self._serial.port
+        if not self._serial.port:
+            raise ValueError("COM port not provided")
+
+        self._serial.open()
+
+    @override
+    def disconnect(self) -> None:
+        """ This method disconnects from the serial port
+
+        Raises:
+            PortNotOpenError: Serial port is not open
+        """
+        if not self._serial.is_open:
+            raise PortNotOpenError("Serial port is not open")
+
+        self._serial.close()
+
+    @override
+    def write(self, data: str) -> None:
+        """ This method writes data to the serial port
+
+        Args:
+            data (str): The data to write
+
+        Raises:
+            PortNotOpenError: Serial port is not open
+        """
+        if not self._serial.is_open:
+            raise PortNotOpenError("Serial port is not open")
+
+        if not data.endswith(self.EOF):
+            data += self.EOF
+
+        self._serial.write(data.encode())
+
+    @override
+    def read(self, read_timeout_s: float = 0.1) -> str:
+        """ This method reads data from the serial port
+
+        Args:
+            read_timeout_s (float, optional): read timeout. Defaults to 0.1.
+
+        Raises:
+            PortNotOpenError: Serial port is not open
+
+        Returns:
+            str: The data read from the serial port
+        """
+        if not self._serial.is_open:
+            raise PortNotOpenError("Serial port is not open")
+
+        self._serial.timeout = read_timeout_s
+        response = self._serial.read_until(self.EOF.encode())
+
+        return response.decode().strip()
+
+    @override
+    def is_connected(self) -> bool:
+        """ This method checks if the device is connected
+
+        Returns:
+            bool: True if the device is connected, False otherwise
+        """
+        return self._serial.is_open
+
+    def read_response(self) -> str:
+        """ This method reads the response from the device
+
+        Returns:
+            str: The response from the device
+        """
+        response = self.read()
+
+        if self.ACK in response:
+            response = response.replace(self.ACK, "")
+        elif self.NACK in response:
+            raise SerialException(f"NACK received: {response}")
+        else:
+            raise SerialException(f"Invalid response: {response}")
+
+        return response
+
+    def write_command(self, command_name: str, *argv) -> str:
+        """ This method writes a command to the serial port and reads the response
+
+        Args:
+            command_name (str): The command to write
+
+        Returns:
+            str: The response from the device
+        """
+        command = command_name
+
+        for arg in argv:
+            command += f" {arg}"
+
+        self.write(command)
+
+        return self.read_response()
