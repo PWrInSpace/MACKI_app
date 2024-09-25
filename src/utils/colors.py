d124c8420d88ec30374d56c85551aaf2e767d798
Kuba <jakkub68@gmail.com>
2024-09-25 17:57:36 +0200

    add colors

---
+++ src/utils/colors.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Colors(Enum):
+    RED = "#FF204E"
+    BLUE = "#A3D8FF"
+    MINT = "#15F5BA"
+    PINK = "#AF47D2"
+    PURPLE = "#B8B5FF"
+    GREEN = "#9BEC00"
+    YELLOW = "#F6FA70"
