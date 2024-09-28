27e0ba10958de7a7e9f006515da0501bff99a285
Kuba <jakkub68@gmail.com>
2024-09-28 14:21:24 +0200

    Feat: data viewer - data text

---
+++ tests/data_viewers/text/test_data_text_basic.py
@@ -0,0 +1,26 @@
+import pytest
+from PySide6.QtWidgets import QHBoxLayout
+from src.data_viewers import DataTextBasic
+
+NAME = "test"
+DEFAULT_VALUE = ""
+
+
+@pytest.fixture
+def data_text():
+    return DataTextBasic(NAME)
+
+
+def test_init(data_text):
+    assert data_text._name == NAME
+    assert data_text._value_label.text() == DEFAULT_VALUE
+
+    assert isinstance(data_text.layout(), QHBoxLayout)
+    assert data_text.layout().itemAt(0).widget().text() == NAME
+    assert data_text.layout().itemAt(1).widget() == data_text._value_label
+
+
+@pytest.mark.parametrize("value", [42, "Hello, World!"])
+def test_update_data(data_text, value):
+    data_text.update_data(value)
+    assert data_text._value_label.text() == str(value)
