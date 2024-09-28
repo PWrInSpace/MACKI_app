664e04e11a27dde7f53a287a9545d5ce8b5160d2
Kuba <jakkub68@gmail.com>
2024-09-28 13:58:42 +0200

    Feat: data_viewers and conftest.py

---
+++ src/data_viewers/__init__.py
@@ -0,0 +1,3 @@
+from src.data_viewers.abstract.data_viewer_basic import DataViewerBasic
+from src.data_viewers.text.data_text_basic import DataTextBasic
+from src.data_viewers.text.data_text_value import DataViewerText
---
+++ src/data_viewers/text/data_viewer_text.py
@@ -0,0 +1,9 @@
+from src.data_viewers.abstract.data_viewer_basic import DataViewerBasic
+
+
+class DataViewerText(DataViewerBasic):
+    def __init__(self, name: str = ""):
+        super().__init__(name)
+
+    def update_data(self, data_dict: dict[str, any]):
+        pass
