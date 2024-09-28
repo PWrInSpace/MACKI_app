d1cb6cfdcd46f2f339f45b6f2c9355434854884c
Kuba <jakkub68@gmail.com>
2024-09-29 00:24:08 +0200

    plots

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ from src.app.cameras_app import QCameraApp
 from src.app.com.macus_widget import MacusWidget
 from src.data_displays import (
     DataDisplayText,
+    DataDisplayPlot,
+    DataPlot,
 )
 
 
@@ -22,28 +24,36 @@ class MainWindow(QMainWindow):
 
         layout = QVBoxLayout()
         self.cameras = QCameraApp()
-        # layout.addWidget(self.cameras)
 
-        # self.macus_widget = MacusWidget()
-        # self.macus_widget.settings_box.setFixedWidth(240)
-        # self.macus_widget.setFixedSize(700, 240)
-        # layout.addWidget(self.macus_widget)
-        self.data_display = DataDisplayText.from_JSON("config/data_text.json")
+        self.data_display_text = DataDisplayText.from_JSON("config/data_text.json")
         data = {
             "title": 32,
             "pressure": 0,
             "valve_state": "1",
         }
-        self.data_display.update_data(data)
-        layout.addWidget(self.data_display)
+        self.data_display_text.update_data(data)
+
+        self.data_display_plot = DataDisplayPlot.from_JSON("config/data_plot.json")
+
+        data = {"pressure": 0, "temperature": 0, "time": 0}
+        self.data_display_plot.update_data(data)
+
+        data = {"pressure": 1, "temperature": 1, "time": 1}
+        self.data_display_plot.update_data(data)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.data_display_plot)
+        layout.addWidget(self.data_display_text)
 
         widget = QWidget()
         widget.setContentsMargins(0, 0, 0, 0)
         widget.setLayout(layout)
 
-        self.setFixedSize(self.sizeHint())
+        # self.setFixedSize(self.sizeHint())
+        # self.setFixedSize(self.sizeHint())
         self.setCentralWidget(widget)
-        self.cameras.enable_cameras()
+        # self.cameras.enable_cameras()
+
 
     def closeEvent(self, event):
         self.cameras.terminate_threads()
