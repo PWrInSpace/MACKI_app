1a4fa4c50e209e46e9a4a05727b8f5c900c46fcd
Kuba <jakkub68@gmail.com>
2024-09-28 17:20:12 +0200

    data text values tests

--- src/data_viewers/text/data_text_values.py
+++ src/data_viewers/text/data_text_values.py
@@ -5,10 +5,21 @@ from src.utils.colors import Colors
 
 class DataTextValues(DataTextBasic):
     def __init__(self, name: str, values: Values) -> None:
+        """Initializes the DataTextValues class.
+
+        Args:
+            name (str): The name of the data.
+            values (Values): The values to display.
+        """
         super().__init__(name)
         self._values = values
 
     def update_data(self, value: str) -> None:
+        """ Updates the data displayed in the widget.
+
+        Args:
+            value (str): The value to display.
+        """
         params = self._values[value]
         if params is None:
             params = DisplayParams(value, Colors.RED)
