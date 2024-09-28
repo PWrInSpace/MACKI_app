466b6374ecbafd21918fc4c04c15c108fed2071f
Kuba <jakkub68@gmail.com>
2024-09-28 17:05:02 +0200

    Add data text values

--- src/data_viewers/text/data_text_values.py
+++ src/data_viewers/text/data_text_values.py
@@ -1,9 +1,17 @@
-from src.data_viewers.abstract.data_viewer_basic import DataViewerBasic
+from src.data_viewers.text.data_text_basic import DataTextBasic
+from src.data_viewers.text.data_text_values_utils import Values, DisplayParams
+from src.utils.colors import Colors
 
 
-class DataTextValues(DataViewerBasic):
-    def __init__(self, name: str = ""):
+class DataTextValues(DataTextBasic):
+    def __init__(self, name: str, values: Values) -> None:
         super().__init__(name)
+        self._values = values
 
-    def update_data(self, data_dict: dict[str, any]):
-        pass
+    def update_data(self, value: str) -> None:
+        params = self._values[value]
+        if params is None:
+            params = DisplayParams(value, Colors.RED)
+
+        self._value_label.setText(params.display_value)
+        self._value_label.setStyleSheet(f"color: {params.color.value}")
