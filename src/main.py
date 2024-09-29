915bbbaadb8b1b1ef17e81ae713d96c8a51e79ae
Kuba <jakkub68@gmail.com>
2024-09-29 14:13:01 +0200

    Excepthook

--- src/main.py
+++ src/main.py
@@ -3,9 +3,21 @@ import logging
 from PySide6.QtWidgets import QApplication
 
 from src.app.app import App
+from PySide6.QtWidgets import QMessageBox
+
+
+def excepthook(exc_type, exc_value, exc_traceback):
+    error_message = f"Exception type: {exc_type.__name__}\nException value: {exc_value}"
+    msg_box = QMessageBox()
+    msg_box.setIcon(QMessageBox.Critical)
+    msg_box.setText("An unexpected error occurred")
+    msg_box.setInformativeText(error_message)
+    msg_box.setWindowTitle("Error")
+    msg_box.exec()
 
 
 def main():
+    sys.excepthook = excepthook
     logging.basicConfig(level=logging.INFO)
     app = QApplication(sys.argv)
 
